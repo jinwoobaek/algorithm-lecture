79b10b593d052c318bd62144d249b5e011d74461
Jinwoo Baek <back3946@gmail.com>
2022-02-17 03:20:13 +0900

    ssh test 3 해결 - mac에서 ssh key 만들때 암호를 만들었기 때문에 push 할때마다 비밀번호 쳐야되는거고, 내 기억에 집 데스크탑에 ssh key 만들때는 암호 안만들고 했었다.

--- src/com/study/Main.java
+++ src/com/study/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("ssh test2");
+        System.out.println("ssh test3");
     }
 }
