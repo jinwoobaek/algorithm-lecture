3c15771abbb1470e7f0df3dfec90b6e648616104
Jinwoo Baek <back3946@gmail.com>
2022-02-17 22:33:48 +0900

    Intertion Sort test

--- src/com/study/sort/InsertionSort.java
+++ src/com/study/sort/InsertionSort.java
@@ -4,5 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class InsertionSort {
-
+    public ArrayList<Integer> sort(ArrayList<Integer> dataList) {
+        for (int i = 0; i < dataList.size() - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (dataList.get(j) < dataList.get(j - 1)) {
+                    Collections.swap(dataList, j, j - 1);
+                } else {
+                    break; // 앞에 안바뀌면 더이상 돌릴 필요 없다.
+                }
+            }
+        }
+        return dataList;
+    }
 }
