d28585fdf8ddd8347500a0bf36cfaa07cc90acc3
Jinwoo Baek <back3946@gmail.com>
2022-02-17 22:41:05 +0900

    Insertion Sort

---
+++ src/com/study/sort/SelectionSort.java
@@ -0,0 +1,21 @@
+package com.study.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SelectionSort {
+    public ArrayList<Integer> sort(ArrayList<Integer> dataList) {
+        int lowest;
+        for (int stand = 0; stand < dataList.size() - 1; stand++) {
+            lowest = stand;
+
+            for (int i = stand + 1; i < dataList.size(); i++) {
+                if (dataList.get(lowest) > dataList.get(i)) {
+                    lowest = i;
+                }
+            }
+            Collections.swap(dataList, lowest, stand);
+        }
+        return dataList;
+    }
+}
